a4ed2f90b54032a57c68e75b75c7c0bea89d130d
Savithri Wewala <s.w.wewala@gmail.com>
2022-11-14 17:53:00 +0900

    made homestays model

---
+++ app/models/homestay.rb
@@ -0,0 +1,3 @@
+class Homestay < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20221114084109_create_homestays.rb
@@ -0,0 +1,17 @@
+class CreateHomestays < ActiveRecord::Migration[7.0]
+  def change
+    create_table :homestays do |t|
+      t.integer :number_of_users
+      t.string :comments
+      t.integer :price
+      t.references :user, null: false, foreign_key: true
+      t.text :family_description
+      t.string :accomodation
+      t.string :address
+      t.boolean :availability
+      t.string :optionals
+
+      t.timestamps
+    end
+  end
+end
