2f3b47430c76f1af22b430cebc4a3fabe4aad733
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-15 11:43:47 +0900

    Done with seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_090631) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_122910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "homestay_id", null: false
+    t.date "booking_start"
+    t.date "booking_end"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["homestay_id"], name: "index_bookings_on_homestay_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "homestays", force: :cascade do |t|
     t.integer "number_of_users"
@@ -39,9 +51,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_090631) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "gender"
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "homestays"
+  add_foreign_key "bookings", "users"
   add_foreign_key "homestays", "users"
 end
