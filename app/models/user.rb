6d6b1093be16c46549f96dfc4055cb6f50367484
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-15 09:01:41 +0900

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings
+  has_one_attached :photo
+
+
+  validates :number_of_users, presence: true, numericality: { less_than_or_equal_to: 4 }
 end
