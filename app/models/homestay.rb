063c9349dd2c7cacc9b72ded8837776f7a2f3cfb
Savithri Wewala <s.w.wewala@gmail.com>
2022-11-15 17:59:08 +0900

    added map placeholder and filter button

--- app/models/homestay.rb
+++ app/models/homestay.rb
@@ -5,6 +5,8 @@ class Homestay < ApplicationRecord
 
   validates :price, presence: true
   validates :address, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
   validates :availability, presence: true
   validates :number_of_users, presence: true, numericality: { less_than_or_equal_to: 4 }
 end
