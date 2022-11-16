670b856f01c2d45026b0ebaa2b1561f2ebcb3878
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-11-16 11:37:17 +0900

    add img to seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_080922) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_022921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_080922) do
     t.datetime "updated_at", null: false
     t.string "country"
     t.string "city"
+    t.string "img_url"
     t.index ["user_id"], name: "index_homestays_on_user_id"
   end
 
