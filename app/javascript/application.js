7932987f80e4300140d01911481fb7fd86a31f66
Ayhem chelly <aihamchelly@gmail.com>
2022-11-18 12:01:31 +0900

    buttons

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,21 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "bootstrap"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "bootstrap";
+
+var animateButton = function (e) {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove("animate");
+
+  e.target.classList.add("animate");
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+  }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener("click", animateButton, false);
+}
