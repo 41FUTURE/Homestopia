560f8f39c87ed8548817df33006cb5a6b8661905
Ayhem chelly <aihamchelly@gmail.com>
2022-11-18 12:18:13 +0900

    some nice changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,20 +2,4 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 import "bootstrap";
-
-var animateButton = function (e) {
-  e.preventDefault;
-  //reset animation
-  e.target.classList.remove("animate");
-
-  e.target.classList.add("animate");
-  setTimeout(function () {
-    e.target.classList.remove("animate");
-  }, 700);
-};
-
-var bubblyButtons = document.getElementsByClassName("bubbly-button");
-
-for (var i = 0; i < bubblyButtons.length; i++) {
-  bubblyButtons[i].addEventListener("click", animateButton, false);
-}
+import "./controllers/button";
